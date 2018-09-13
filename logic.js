e5a273f5484f09459c281ba28f73e514de7de4a7
ArshSharifat <arshiasharifat@gmail.com>
2018-09-12 19:55:15 -0700

    GIFS populate onto html

--- logic.js
+++ logic.js
@@ -1,16 +1,5 @@
-var topics = ["justin timberlake", "jamie fox", "lebron james", "mila kunis", "the rock", "kim kardashian", "jennifer lopez", "cardi b", "arnold schwarzenegger"]
-// addButtons();
-// function addButtons() {
-//     topics.forEach(function (topicName) {
-//         // create a new button
-//         var newButton = $("<button>");
-//         // add values to the new button from our array
-//         newButton.html(topicName);
-//         // append the new button to the page
-//         $("#root").append(newButton);
-//     })
-
-// }
+var topics = ["justin timberlake", "jamie fox", "lebron james", "mila kunis",
+    "the rock", "kim kardashian", "jennifer lopez", "cardi b", "arnold schwarzenegger"]
 
 addButtons();
 function addButtons() {
@@ -20,7 +9,7 @@ function addButtons() {
         // add values to the new button from our array
         newButton.html(topicName);
         newButton.addClass("buttons");
-        newButton.on("click",function(){
+        newButton.on("click", function () {
             var itemValue = topicName;
             console.log(itemValue);
         })
@@ -29,6 +18,51 @@ function addButtons() {
     })
 
 }
+
+//on click variable to retrieve celebrity name from button for query URL
+var name = $(".buttons").click(function () {
+    console.log(this.innerHTML);
+    name = this.innerHTML;
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    name + "&api_key=8hSW7KTGpdnQtB2M4voxHQE9E9pscp04&limit=10&rating=g";
+    
+    // AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var personImage = $("<img>");
+          personImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(personImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+        }
+        
+    });
+    
+});
+
+    
+
+
+
+
+
+
+
 
 
 
